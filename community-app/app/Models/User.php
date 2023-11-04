6f9b8b10759f7dfef9d5d92682dd5e1ccaceef4c
Lainari <108247620+Lainari@users.noreply.github.com>
2023-11-04 20:39:25 +0900

    refactor : add user_id in $fillable

--- community-app/app/Models/User.php
+++ community-app/app/Models/User.php
@@ -9,6 +9,7 @@ class User extends Authenticatable implements JWTSubject
 {
     protected $fillable = [
         'id',
+        'user_id',
         'nickname',
         'password', 
         'birthday'
