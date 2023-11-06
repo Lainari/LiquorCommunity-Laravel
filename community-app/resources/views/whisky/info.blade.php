e0288546631de7923cc9e068ce4c594c3e63aa9e
Lainari <108247620+Lainari@users.noreply.github.com>
2023-11-06 23:08:15 +0900

    style : update style in whisky info & info post

--- community-app/resources/views/whisky/info.blade.php
+++ community-app/resources/views/whisky/info.blade.php
@@ -4,20 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/whisky/info.css') }}">
     <title>위스키 정보찾기</title>
 </head>
 <body>
     @include('common/header')
-    위스키 정보
-    <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#infoModal">작성</button>
 
-    @foreach ($posts as $post)
-    <div class="post">
-        <h2><a href="/whisky/info/{{ $post->id }}">{{ $post->title }}</a></h2>
-        <p>{{ $post->content }}</p>
-        <p>작성자: {{ $post->nickname }}</p>
+    <div class="d-flex justify-content-center">
+        <div class="feat-box justify-content-end">
+            <button class="btn btn-primary create-btn" type="button" data-bs-toggle="modal" data-bs-target="#infoModal">작성</button>
+        </div>
     </div>
-    @endforeach
+    <div class="d-flex justify-content-center">
+        @foreach ($posts as $post)
+        <div class="post justify-content-start">
+            <a href="/whisky/info/{{ $post->id }}">
+                <img class="img-thumbnail thumbnail" src="{{asset(json_decode($post->image, true)[0])}}">
+            </a>
+            <div class="m-2">
+                <h3><a class="post-title" href="/whisky/info/{{ $post->id }}">{{ $post->title }}</a></h3>
+                <p class="post-writer">작성자 / {{ $post->nickname }}</p>
+            </div>
+        </div>
+        @endforeach
+    </div>
 
     @include('whisky/modal/infoModal')
     @include('common/footer')
--- community-app/resources/views/whisky/post/info_post.blade.php
+++ community-app/resources/views/whisky/post/info_post.blade.php
@@ -4,17 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/whisky/infoPost.css') }}">
     <title>{{$post->title}}</title>
 </head>
 <body>
     @include('common/header')
-    <div class="post">
-        <h2>제목 : {{ $post->title }}</h2>
-        <p>{{ $post->content }}</p>
-        <p>작성자: {{ $post->nickname }}</p>
-        @foreach(json_decode($post->image, true) as $image)
-            <img src="{{ asset($image) }}">
-        @endforeach
+    <div class="d-flex justify-content-center">
+        <div class="post-box justify-content-start">
+            <div class="border rounded mt-3 p-1 d-flex">
+                <span class="fs-3 fw-bold pt-2 pb-2">제목 : {{ $post->title }}</span>
+            </div>
+            <div class="border-bottom mt-3">
+                <p class="fs-5 fw-bolder">작성자: {{ $post->nickname }}</p>
+            </div>
+            <div class="border rounded mt-4">
+                <div class="text-center">
+                    @foreach(json_decode($post->image, true) as $image)
+                        <img class="img-box mt-5" src="{{ asset($image) }}">
+                    @endforeach
+                </div>
+                <p class="fs-5 p-3 lh-base">{{ $post->content }}</p>
+            </div>
+        </div>
     </div>
     @include('common/footer')
 </body>
