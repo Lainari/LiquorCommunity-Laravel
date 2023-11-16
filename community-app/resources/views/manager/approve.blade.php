cc20609c641135fba9dad7b1281a501410d12225
Lainari <108247620+Lainari@users.noreply.github.com>
2023-11-16 20:38:41 +0900

    refactor : add approve blade page

---
+++ community-app/resources/views/manager/approve.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>게시글 승인</title>
+</head>
+<body>
+    @include('common/header')
+    게시글 승인 페이지
+    @include('common/footer')
+</body>
+</html>
