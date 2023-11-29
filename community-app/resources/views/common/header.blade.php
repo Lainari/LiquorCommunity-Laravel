68ee913ec33026bd288d2a9aaabe6167a9520f30
Lainari <108247620+Lainari@users.noreply.github.com>
2023-11-29 23:18:39 +0900

    chor : update bootstrap version

--- community-app/resources/views/common/header.blade.php
+++ community-app/resources/views/common/header.blade.php
@@ -71,6 +71,6 @@
 
 {{-- dropdown 의존성을 위한 jquery와 popper.js 주입 --}}
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="{{asset('/js/service/logout/index.js')}}"></script>
 <script src="{{asset('/js/service/post/search/index.js')}}"></script>
