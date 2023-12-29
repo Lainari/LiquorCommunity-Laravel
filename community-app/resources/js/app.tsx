6b626371253514e90cbb5ba8022ebbe46798758b
Lainari <108247620+Lainari@users.noreply.github.com>
2023-12-29 23:39:36 +0900

    refactor : retype js to tsx for use react framework

---
+++ community-app/resources/js/app.tsx
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+// Import the main component
+import Main from './components/Main';
+
+// Then, use it in your application
+ReactDOM.render(<Main />, document.getElementById('app'));
