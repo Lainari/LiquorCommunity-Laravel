54af50f9c0533a4aac9a6c114bee09145b2be101
Lainari <108247620+Lainari@users.noreply.github.com>
2024-01-02 22:03:07 +0900

    refactor : use Inertia in AppServiceProvide

--- community-app/app/Providers/AppServiceProvider.php
+++ community-app/app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Inertia\Inertia;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -19,10 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Inertia::version(function () {
+            return md5_file(public_path('mix-manifest.json'));
+        });
         // 모든 view에 대해 사용자 정보 접근 가능
         view()->composer('*', function ($view) {
             $user = auth()->user();
             $view->with('user', $user);
         });
+        
     }
 }
