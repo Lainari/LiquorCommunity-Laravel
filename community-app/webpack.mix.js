862680c5e42747385d8647e40e9c65993cd33493
Lainari <108247620+Lainari@users.noreply.github.com>
2024-01-03 21:24:09 +0900

    refactor : delete webpack because it doens't use laravel mix

--- community-app/webpack.mix.js
+++
@@ -1,7 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.sass('resources/css/app.scss', 'public/css');
-
-mix.js('resources/js/app.js', 'public/js')
-   .react()
-   .browserSync('localhost:8000');
