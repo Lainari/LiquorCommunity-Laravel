030eafca971529582561b91cb3adf29d801c315b
Lainari <108247620+Lainari@users.noreply.github.com>
2024-01-10 21:43:39 +0900

    refactor : delete file because not use anymore

--- community-app/resources/js/app.tsx
+++
@@ -1,7 +0,0 @@
-import './bootstrap';
-
-// Import the main component
-import Main from './components/Main';
-
-// Then, use it in your application
-ReactDOM.render(<Main />, document.getElementById('app'));
