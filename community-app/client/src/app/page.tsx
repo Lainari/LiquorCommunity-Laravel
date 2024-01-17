181d42d06c4b39bcfd19e435ab2aed8ad8797655
Lainari <108247620+Lainari@users.noreply.github.com>
2024-01-17 22:24:01 +0900

    refactor : add 'use client' method and use db connect

--- community-app/client/src/app/page.tsx
+++ community-app/client/src/app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Image from 'next/image';
 import RootLayout from './layout';
+import { getAdminNickname } from '../services';
 
 const Home: NextPage = () => {
+  const [adminNickname, setAdminNickname] = useState('');
+
+  useEffect(() => {
+    getAdminNickname().then(data => setAdminNickname(data));
+  }, []);
+
   return (
     <RootLayout>
       <div className="container mx-auto">
@@ -11,16 +20,11 @@ const Home: NextPage = () => {
           <div className="w-1/2">
             <h2 className="text-2xl">What is WhiskyCommunity?</h2>
             <p className="my-4">
-              이 곳, 위스키 커뮤니티(WhiskyCommunity)는 <br />
-              세계 어느 술이면 다 좋은 애주가들이 <br />
-              자신이 아는 위스키의 정보를 공유하고 <br />
-              혼자서 탐구하거나 여럿이 즐길 수 있는 <br />
-              주류 문화 커뮤니티 사이트입니다.
+              데이터 베이스 확인
             </p>
-            <button className="btn btn-info">위스키 보러가기</button>
           </div>
           <div className="w-1/2">
-            이미지 들어갈 곳
+            관리자 명 : {adminNickname}
           </div>
         </div>
       </div>
