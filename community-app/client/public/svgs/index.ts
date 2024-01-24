d3dbc86c144e786931b1ddd083a5ca98613b1342
Lainari <108247620+Lainari@users.noreply.github.com>
2024-01-24 20:24:15 +0900

    feat : add svg file to use main layout.tsx

--- community-app/client/public/svgs/index.ts
+++ community-app/client/public/svgs/index.ts
@@ -1,7 +1,9 @@
 import Iconnextsvg from './next.svg'
 import Iconvercel from './vercel.svg'
+import Mainicon from './whisky-whiskey-svgrepo-com.svg'
 
 export {
     Iconnextsvg,
     Iconvercel,
+    Mainicon,
 };
