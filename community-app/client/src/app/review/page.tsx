504e62771ae38bf91ce4c427603626fa3a870651
Lainari <108247620+Lainari@users.noreply.github.com>
2024-02-07 23:57:44 +0900

    Refactor: File move to src/app not used pages directory

---
+++ community-app/client/src/app/review/page.tsx
@@ -0,0 +1,9 @@
+import RootLayout from '@/app/layout';
+
+export default function Review() {
+  return (
+    <RootLayout>
+      <>Review Page</>
+    </RootLayout>
+  );
+}
--- community-app/client/src/pages/whisky/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function Layout({children}: {children: React.ReactNode}) {
-  return (
-    <form action="">
-      <h2 className="text-4xl">Whisky</h2>
-      {children}
-    </form>
-  );
-}
