83721a24f7b9427106297bdb8cd5409ced441280
Lainari <108247620+Lainari@users.noreply.github.com>
2024-02-08 20:49:09 +0900

    Refactor: Not used RootLayout in Review page

--- community-app/client/src/app/review/page.tsx
+++ community-app/client/src/app/review/page.tsx
@@ -1,9 +1,3 @@
-import RootLayout from '@/app/layout';
-
 export default function Review() {
-  return (
-    <RootLayout>
-      <>Review Page</>
-    </RootLayout>
-  );
+  return <>Review Page</>;
 }
