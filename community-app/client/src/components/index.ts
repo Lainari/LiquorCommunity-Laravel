8a444a5e6f009a900ba83936b437a9563a9d751a
Lainari <108247620+Lainari@users.noreply.github.com>
2024-02-14 22:32:46 +0900

    🔨 Refactor : Add login, logout components in components/index.ts

--- community-app/client/src/components/index.ts
+++ community-app/client/src/components/index.ts
@@ -1,4 +1,6 @@
 import Footer from './footer';
 import Header from './header';
+import Login from './login';
+import Logout from './logout';
 
-export {Header, Footer};
+export {Header, Footer, Login, Logout};
