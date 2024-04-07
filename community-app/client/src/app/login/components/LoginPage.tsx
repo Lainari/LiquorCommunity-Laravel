653ff86b5cb311f4f2cefe4673f3debf09c78256
Lainari <108247620+Lainari@users.noreply.github.com>
2024-04-07 23:17:51 +0900

    ♻️ refactor : Modify login page

--- community-app/client/src/app/login/components/LoginPage.tsx
+++ community-app/client/src/app/login/components/LoginPage.tsx
@@ -1,38 +1,27 @@
-'use client';
-
 import Image from 'next/image';
-import {useEffect} from 'react';
 import loginRequest from '@/app/api/loginRequest';
+import styles from './LoginPage.module.css';
 
 const LoginPage = () => {
   const handleGoogleLogin = () => {
     loginRequest();
   };
 
-  useEffect(() => {
-    const loginButton = document.getElementById('googleLoginButton');
-    if (loginButton) {
-      loginButton.addEventListener('click', handleGoogleLogin);
-
-      return () => {
-        loginButton.removeEventListener('click', handleGoogleLogin);
-      };
-    } else {
-      return;
-    }
-  }, []);
-
   return (
-    <div className="flex justify-center items-center">
-      <div className="w-2/3 flex bg-white rounded-lg shadow border md:mt-0 xl:p-0 mr-3">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 flex-1">
+    <div className={styles.container}>
+      <div
+        className={`${styles.card} flex bg-white rounded-lg shadow border md:mt-0 xl:p-0 mr-3`}
+      >
+        <div
+          className={`${styles.content} p-6 space-y-4 md:space-y-6 sm:p-8 flex-1`}
+        >
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
             Sign in to your account
           </h1>
           <div className="flex justify-between">
             <h1>This is WhiskyCommunity</h1>
             <Image
-              className="flex sm:max-w-md rounded-lg shadow border md:mt-0 xl:p-0"
+              className={`${styles.image} flex sm:max-w-md rounded-lg shadow border md:mt-0 xl:p-0`}
               src="/images/loginImage.png"
               width={500}
               height={500}
@@ -40,8 +29,8 @@ const LoginPage = () => {
             />
           </div>
           <button
-            id="googleLoginButton"
-            className="w-full text-white bg-black hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className={`${styles.button} w-full text-white bg-black hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center`}
+            onClick={handleGoogleLogin}
           >
             Sign in with Google
           </button>
