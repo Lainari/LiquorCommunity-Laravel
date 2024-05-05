14069be937615d521727554cfa6d8d25981432c3
Lainari <108247620+Lainari@users.noreply.github.com>
2024-05-06 06:09:08 +0900

    ✨ Feat : Add image config file
    - Modify column name in image model

--- community-app/app/Models/Image.php
+++ community-app/app/Models/Image.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
-    protected $fillable = ['post_id', 'path', 'profile_image'];
+    protected $fillable = ['post_id', 'path', 'profile'];
 }
