afbeb3db8cfe092e86d1efccafd66e7722a81b3d
Lainari <108247620+Lainari@users.noreply.github.com>
2024-05-13 22:44:16 +0900

    💄 Style : Modify Login page style

--- community-app/client/src/app/login/components/LoginPage.tsx
+++ community-app/client/src/app/login/components/LoginPage.tsx
@@ -10,15 +10,17 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="mt-4 w-1/2 flex bg-white rounded-lg shadow border mb-4">
+    <div className="mt-4 w-1/2 flex items-center bg-white rounded-lg shadow border mb-4">
       <div className="p-6 space-y-4 flex-1">
-        <p className="text-3xl text-center font-bold leading-tight tracking-tight text-gray-900">
-          Sign in to your account
-        </p>
-        <p className="text-center font-bold text-gray-400">
-          You can enjoy to use social signin, Join us!
-        </p>
-        <div className="flex justify-center">
+        <div className="mb-10">
+          <p className="text-3xl text-center font-bold leading-tight tracking-tight text-gray-900">
+            Sign in to your account
+          </p>
+          <p className="text-center font-bold text-gray-400">
+            You can enjoy to use social signin, Join us!
+          </p>
+        </div>
+        <div className="flex justify-center mb-4">
           <Image
             className="flex sm:max-w-md rounded-lg shadow border md:mt-0 xl:p-0"
             src={loginPageImages.pub}
@@ -29,7 +31,7 @@ const LoginPage = () => {
         </div>
         <div className="flex justify-center">
           <div
-            className="w-3/4 text-white bg-black hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className="w-3/4 text-white bg-black hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-10"
             onClick={handleGoogleLogin}
           >
             Sign in with Google
